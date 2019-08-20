68bfba8ee123d3c00b86267926704dce48feb12b
Daniel_Blythe <daniel.blythe@2112comms.com>
2019-08-20 21:38:16 +0100

    added strawberry thumbs up pic. :) and dashboard

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -4,8 +4,11 @@ import { Link } from 'react-router-dom';
 export default function Dashboard() {
 	return (
 		<div>
-			<h1>Welcome to Fruitful Surveys</h1>
-			<Link to="/surveys">View your surveys</Link>
+			<h1 className="text-center my-5">Welcome to Fruitful Surveys</h1>
+			<img
+				className="rounded-full mx-auto block"
+				src="http://theendearingdesigner.com/wp-content/uploads/2015/07/Creative-Fruits-Vegetables-Image-640x401.jpg"
+			/>
 		</div>
 	);
 }
